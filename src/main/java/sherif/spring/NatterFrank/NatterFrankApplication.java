fedf8dbea298859775ed74bc6642e6c66ee6bf99
sherif-med <35470250+sherif-med@users.noreply.github.com>
2018-12-21 18:20:36 +0100

    Dependencies add

---
+++ src/main/java/sherif/spring/NatterFrank/NatterFrankApplication.java
@@ -0,0 +1,14 @@
+package sherif.spring.NatterFrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatterFrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatterFrankApplication.class, args);
+	}
+
+}
+
